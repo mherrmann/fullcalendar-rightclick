c2cecbfa182b27d93a4c40a602d97ee77ef5949d
Michael Herrmann <michael@herrmann.io>
2015-06-11 19:36:51 +0200

    Add support for FullCalendar's month view.

--- fullcalendar-rightclick.js
+++ fullcalendar-rightclick.js
@@ -1,34 +1,39 @@
 /*!
- * fullcalendar-rightclick v1.1
+ * fullcalendar-rightclick v1.2
  * Docs & License: https://github.com/mherrmann/fullcalendar-rightclick
  * (c) 2015 Michael Herrmann
  */
 
 (function($) {
-	var fc = $.fullCalendar;
-	var AgendaView = fc.views.agenda;
-	var originalRender = AgendaView.prototype.render;
-	AgendaView.prototype.render = function() {
-		originalRender.call(this);
-		this.registerDayRightclickListener();
-		this.registerEventRightclickListener();
-	};
-	AgendaView.prototype.registerDayRightclickListener = function() {
-		var that = this;
-		this.el.on('contextmenu', '.fc-widget-content .fc-slats',
-			function(ev) {
-				that.coordMap.build();
-				var cell = that.coordMap.getCell(ev.pageX, ev.pageY);
-				if (cell)
-					return that.trigger('dayRightclick', null, cell.start, ev);
-			}
-		);
-	};
-	AgendaView.prototype.registerEventRightclickListener = function() {
-		var that = this;
-		this.el.on('contextmenu', '.fc-event-container > *', function(ev) {
-			var seg = $(this).data('fc-seg');
-			return that.trigger('eventRightclick', this, seg.event, ev);
-		});
+	function monkeyPatchViewClass(View, dayCssClass) {
+		var originalRender = View.prototype.render;
+		View.prototype.render = function() {
+			originalRender.call(this);
+			this.registerDayRightclickListener();
+			this.registerEventRightclickListener();
+		};
+		View.prototype.registerDayRightclickListener = function() {
+			var that = this;
+			this.el.on('contextmenu', '.fc-widget-content ' + dayCssClass,
+				function(ev) {
+					that.coordMap.build();
+					var cell = that.coordMap.getCell(ev.pageX, ev.pageY);
+					if (cell)
+						return that.trigger(
+							'dayRightclick', null, cell.start, ev
+						);
+				}
+			);
+		};
+		View.prototype.registerEventRightclickListener = function() {
+			var that = this;
+			this.el.on('contextmenu', '.fc-event-container > *', function(ev) {
+				var seg = $(this).data('fc-seg');
+				return that.trigger('eventRightclick', this, seg.event, ev);
+			});
+		}
 	}
+	var fc = $.fullCalendar;
+	monkeyPatchViewClass(fc.views.agenda, '.fc-slats');
+	monkeyPatchViewClass(fc.views.basic, '.fc-content-skeleton');
 })(jQuery);
